c69edb1417aabb0f92a0274ee094ef6e7543b729
Sean <sean.stavro@gmail.com>
2015-09-08 11:27:42 -0700

    Delegate file naming to Versioning module for local storage

--- lib/arc/storage/local.ex
+++ lib/arc/storage/local.ex
@@ -1,22 +1,24 @@
 defmodule Arc.Storage.Local do
-
   def put(definition, version, {file, scope}) do
     destination_dir = definition.storage_dir(version, {file, scope})
     File.mkdir_p(destination_dir)
-    file_name = definition.filename(version, file)
-    {:ok, _} = File.copy(file.path, Path.join(destination_dir, file_name))
-    file_name
+    {:ok, _} = File.copy(file.path, Path.join(destination_dir, file.file_name))
+    file.file_name
   end
 
-  def url(definition, version, {file, scope}, options \\ []) do
-    destination_dir = definition.storage_dir(version, {file, scope})
-    file_name = definition.filename(version, file)
-    Path.join(destination_dir, file_name)
+  def url(definition, version, file_and_scope, options \\ []) do
+    build_local_path(definition, version, file_and_scope)
   end
 
-  def delete(definition, version, {file, scope}) do
-    destination_dir = definition.storage_dir(version, {file, scope})
-    file_name = definition.filename(version, file)
-    File.rm(Path.join(destination_dir, file_name))
+  def delete(definition, version, file_and_scope) do
+    build_local_path(definition, version, file_and_scope)
+    |> File.rm
+  end
+
+  defp build_local_path(definition, version, file_and_scope) do
+    Path.join([
+      definition.storage_dir(version, file_and_scope),
+      Arc.Definition.Versioning.resolve_file_name(definition, version, file_and_scope)
+    ])
   end
 end
