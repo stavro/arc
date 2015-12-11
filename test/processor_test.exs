8dc85a215b7b481e30530cdc7fa46ee02e3da498
Sean Stavropoulos <sean.stavro@gmail.com>
2015-12-11 12:16:42 -0800

    Handle imagemagick exit codes

--- test/processor_test.exs
+++ test/processor_test.exs
@@ -12,6 +12,16 @@ defmodule ArcTest.Processor do
     def __versions, do: [:original, :thumb]
   end
 
+  defmodule BrokenDefinition do
+    use Arc.Actions.Store
+    use Arc.Definition.Storage
+
+    def validate({file, _}), do: String.ends_with?(file.file_name, ".png")
+    def transform(:original, _), do: {:noaction}
+    def transform(:thumb, _), do: {:convert, "-strip -invalidTransformation 10x10"}
+    def __versions, do: [:original, :thumb]
+  end
+
   test "returns the original path for {:noaction} transformations" do
     assert Arc.Processor.process(DummyDefinition, :original, {Arc.File.new(@img), nil}).path == @img
   end
@@ -22,6 +32,12 @@ defmodule ArcTest.Processor do
     assert "128x128" == geometry(@img) #original file untouched
     assert "10x10" == geometry(new_file.path)
     cleanup(new_file.path)
+  end
+
+  test "raises an error in an invalid transformation" do
+    assert_raise Arc.ConvertError, ~r"unrecognized option", fn ->
+      Arc.Processor.process(BrokenDefinition, :thumb, {Arc.File.new(@img), nil})
+    end
   end
 
   defp geometry(path) do
