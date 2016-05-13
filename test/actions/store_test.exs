2be0254ce4bd7959f6df425e73e8a0c154e90790
Dawid Cichuta <cichaczem@gmail.com>
2016-05-13 14:43:58 +0200

    Use if instead of case when handling true/false results

--- test/actions/store_test.exs
+++ test/actions/store_test.exs
@@ -8,9 +8,10 @@ defmodule ArcTest.Actions.Store do
     use Arc.Definition.Storage
 
     def validate({file, _}) do
-      case String.ends_with?(file.file_name, ".png") do
-        true -> {:ok, :ok}
-        false -> {:error, :invalid_file}
+      if String.ends_with?(file.file_name, ".png") do
+        {:ok, :ok}
+      else
+        {:error, :invalid_file}
       end
     end
 
