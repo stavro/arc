7d672b2f0ba9438ce7e1091fbae1cf408195e45c
Tute Costa <tutecosta@gmail.com>
2016-08-17 08:42:18 -0400

    Drop unused ConvertError exception
    Dropped its mentions in https://github.com/stavro/arc/pull/92.

--- lib/arc/exceptions.ex
+++ lib/arc/exceptions.ex
@@ -1,19 +1,3 @@
-defmodule Arc.ConvertError do
-  defexception [:message]
-
-  def exception(opts) do
-    message = Keyword.fetch!(opts, :message)
-    exit_code = Keyword.fetch!(opts, :exit_code)
-
-    msg = """
-    Convert exited unsuccessfully with exit code #{exit_code}:
-    #{message}
-    """
-
-    %__MODULE__{message: msg}
-  end
-end
-
 defmodule Arc.MissingExecutableError do
   defexception [:message]
 
